41ab83d8ac92f9c1d040da2d13e058ccf5c81317
Islam <islambou@gmail.com>
2018-11-14 15:17:35 +0100

    using antd login form

--- src/components/loginform.js
+++ src/components/loginform.js
@@ -1,110 +1,67 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { FETCH_USER } from "../sagas/types";
+import { Form, Icon, Input, Button, Checkbox } from "antd";
+const FormItem = Form.Item;
 
-export class Loginform extends Component {
-  constructor(props) {
-    super(props);
-    this.username_input_ref = React.createRef();
-    this.password_input_ref = React.createRef();
-  }
-  handleLoginButton = () => {
-    let username = this.username_input_ref.current.value;
-    let pass = this.password_input_ref.current.value;
-    this.props.sendCreds({ username: username, password: pass });
+class Loginform extends React.Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+        this.props.sendCreds(values);
+      }
+    });
   };
+
   render() {
+    console.log(this.props);
+    const { getFieldDecorator } = this.props.form;
     return (
-      <div>
-        <div className="container pt-lg-md">
-          <div className="row justify-content-center">
-            <div className="col-lg-5">
-              <div className="card bg-secondary shadow border-0">
-                <div className="card-body px-lg-5 py-lg-5">
-                  <div className="text-center text-muted mb-4">
-                    <small>Sign in</small>
-                  </div>
-                  <div className="form-group mb-3">
-                    <div className="input-group input-group-alternative">
-                      <div className="input-group-prepend">
-                        <span className="input-group-text">
-                          <i className="ni ni-single-02" />
-                        </span>
-                      </div>
-                      <input
-                        ref={this.username_input_ref}
-                        className="form-control"
-                        placeholder="Email"
-                        type="email"
-                      />
-                    </div>
-                  </div>
-                  <div className="form-group">
-                    <div className="input-group input-group-alternative">
-                      <div className="input-group-prepend">
-                        <span className="input-group-text">
-                          <i className="ni ni-lock-circle-open" />
-                        </span>
-                      </div>
-                      <input
-                        ref={this.password_input_ref}
-                        className="form-control"
-                        placeholder="Password"
-                        type="password"
-                      />
-                    </div>
-                  </div>
-                  <div className="custom-control custom-control-alternative custom-checkbox">
-                    <input
-                      className="custom-control-input"
-                      id=" customCheckLogin"
-                      type="checkbox"
-                    />
-                    <label
-                      className="custom-control-label"
-                      htmlFor=" customCheckLogin"
-                    >
-                      <span>Remember me</span>
-                    </label>
-                  </div>
-                  <div className="text-center">
-                    <button
-                      type="button"
-                      className="btn btn-primary my-4"
-                      onClick={this.handleLoginButton}
-                    >
-                      Sign in
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <div className="row mt-3">
-                <div className="col-6">
-                  <a href="#" className="text-light">
-                    <small>Forgot password?</small>
-                  </a>
-                </div>
-                <div className="col-6 text-right">
-                  <a href="#" className="text-light">
-                    <small>Create new account</small>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Form onSubmit={this.handleSubmit} className="login-form">
+        <FormItem>
+          {getFieldDecorator("username", {
+            rules: [{ required: true, message: "Please input your username!" }]
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="Username"
+            />
+          )}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator("password", {
+            rules: [{ required: true, message: "Please input your Password!" }]
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+              type="password"
+              placeholder="Password"
+            />
+          )}
+        </FormItem>
+        <FormItem>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Log in
+          </Button>
+        </FormItem>
+      </Form>
     );
   }
 }
-
 const mapStateToProps = state => ({});
 
 const mapDispatchToProps = dispatch => ({
   sendCreds: creds => dispatch({ type: FETCH_USER, payload: creds })
 });
+const WrappedLoginForm = Form.create()(Loginform);
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(Loginform);
+)(WrappedLoginForm);
--- src/routes/login.js
+++ src/routes/login.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Loginform from "../components/loginform";
 
-export default function login() {
+export default function Login() {
   return (
-    <div>
-      <Loginform />
+    <div style={{ textAlign: "center" }}>
+      <div style={{ maxWidth: "400px", textAlign: "left", margin: "auto" }}>
+        <Loginform />
+      </div>
     </div>
   );
 }
