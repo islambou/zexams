d9f850a1dd0a17e5b61e3c01b07e7b5865072873
Islam <islambou@gmail.com>
2018-11-14 15:28:24 +0100

    add login route

--- src/appRoutes.js
+++ src/appRoutes.js
@@ -1,20 +1,42 @@
 import { Route } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 
+import { connect } from "react-redux";
 import React, { lazy, Suspense } from "react";
 import Questions from "./routes/questions";
 import AddQuestions from "./components/addQuestion";
 import Tests from "./routes/tests";
+import Login from "./routes/login";
 import displayTest from "./routes/displayTest";
-
+import LoginRedirector from "./components/LoginRedirector";
 //routes
-const Routes = () => (
-  <>
-    <div style={{ marginTop: "50px" }} />
-    <Route path="/questions/:test?/:testName?" component={Questions} />
-    <Route path="/add-question" component={AddQuestions} />
-    <Route path="/tests" component={Tests} />
-    <Route path="/test/:id?" component={displayTest} />
-  </>
-);
+const Routes = props => {
+  const user = props.user;
+  console.log(user == true);
+  return (
+    <>
+      <div style={{ marginTop: "50px" }} />
+      <Route
+        path="/questions/:test?/:testName?"
+        component={user._id != undefined ? Questions : LoginRedirector}
+      />
+      <Route path="/add-question" component={AddQuestions} />
+      <Route path="/tests" component={Tests} />
+      <Route path="/test/:id?" component={displayTest} />
+      <Route path="/login" component={Login} />
+    </>
+  );
+};
 
-export default Routes;
+const mapStateToProps = state => ({
+  user: state.user
+});
+
+const mapDispatchToProps = {};
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Routes)
+);
